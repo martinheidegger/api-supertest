df33d03e4c432d2c9a784dedacdb7c53a0176aee
Martin Heidegger <martin.heidegger@gmail.com>
2018-04-27 12:12:36 +0900

    standard linting

--- output/console.js
+++ output/console.js
@@ -1,98 +1,81 @@
-"use strict";
+'use strict'
 
-var colors = process.env.NO_COLORS ? {
-        gray: passText,
-        bold: passText,
-        red: passText,
-        green: passText
-	} : require("colors/safe");
-
-function passText(text) {
-	return text;
+function passText (text) {
+  return text
 }
 
-function processStack(error) {
+const colors = process.env.NO_COLORS ? {
+  gray: passText,
+  bold: passText,
+  red: passText,
+  green: passText
+} : require('colors/safe')
 
-        if (error.stack) {
-                if (process.env.NO_STACK) {
-                        return error.message;
-                }
-                return error.stack.replace(/^(\s+at\s+.*)$/mig, function (match) {
-                        return colors.gray(match);
-                });
+function processStack (error) {
+  if (error.stack) {
+    if (process.env.NO_STACK) {
+      return error.message
+    }
+    return error.stack.replace(/^(\s+at\s+.*)$/mig, function (match) {
+      return colors.gray(match)
+    })
+  }
+  return error
+}
+
+function indent (text, indent) {
+  return indent + text.split('\n').join('\n' + indent)
+}
+
+function renderPrefix (item) {
+  var result = colors.bold(item.path)
+  if (item.data) {
+    result += colors.gray(' (' + item.method + ' ' + item.data + ')')
+  } else if (item.method !== 'GET') {
+    result += colors.gray(' (' + item.method + ')')
+  }
+  if (item.requestHeader) {
+    var headers = ''
+    Object.keys(item.requestHeader).forEach(function (key) {
+      var value = item.requestHeader[key]
+      if (value) {
+        if (headers !== '') {
+          headers += '; '
         }
-        return error;
-}
-
-function indent(text, indent) {
-        return indent + text.split("\n").join("\n" + indent);
-}
-
-function renderPrefix(item) {
-        var result = colors.bold(item.path);
-        if (item.data) {
-                result += colors.gray(" (" + item.method + " " + item.data + ")");
-        } else if (item.method !== "GET") {
-                result += colors.gray(" (" + item.method + ")")
-        }
-        return result + ": ";
-}
-
-function indent(text, indent) {
-	return indent + text.split("\n").join("\n" + indent);
-}
-
-function renderPrefix(item) {
-	var result = colors.bold(item.path);
-	if (item.data) {
-		result += colors.gray(" (" + item.method + " " + item.data + ")");
-	} else if (item.method !== "GET") {
-		result += colors.gray(" (" + item.method + ")")
-	}
-	if (item.requestHeader) {
-		var headers = "";
-		Object.keys(item.requestHeader).forEach(function (key) {
-			var value = item.requestHeader[key];
-			if (value) {
-				if (headers !== "") {
-					headers += "; ";
-				}
-				headers += key + " " + value	
-			}
-		})
-		if (headers !== "") {
-			result += colors.gray(" [" + headers + "]")	
-		}
-	}
-	return result + ": ";
+        headers += key + ' ' + value
+      }
+    })
+    if (headers !== '') {
+      result += colors.gray(' [' + headers + ']')
+    }
+  }
+  return result + ': '
 }
 
 module.exports = {
-	start: function start(base) {
-		console.log("Running tests on " + colors.gray(base) + "\n");
-	},
-	end: function end(passed, total, cb) {
-		process.nextTick(function () {
-		    cb();
-		});
-		var isSuccess = passed === total;
-		console.log("\nTests " + (isSuccess ? "successful" : "failed" ) + ". (" + passed + "/" + total + ")");
-	},
-	endpointStart: function endpointStart(item) {
-		process.stdout.write(renderPrefix(item) + colors.gray("processing..."));
-	},
-	endpointEnd: function endpointEnd(state) {
-		var item = state.item,
-			error = state.error;
-		process.stdout.clearLine && process.stdout.clearLine();
-		process.stdout.cursorTo && process.stdout.cursorTo(0);
-		process.stdout.write(renderPrefix(item));
-		if (error) {
-			console.log(colors.red("ERROR"));
-			error = processStack(error);
-			console.log("\n" + indent(error, "    ") + "\n");	
-		} else {
-			console.log(colors.green("OK"));
-		}
-	}
-};
+  start: function start (base) {
+    console.log('Running tests on ' + colors.gray(base) + '\n')
+  },
+  end: function end (passed, total, cb) {
+    process.nextTick(cb)
+    var isSuccess = passed === total
+    console.log('\nTests ' + (isSuccess ? 'successful' : 'failed') + '. (' + passed + '/' + total + ')')
+  },
+  endpointStart: function endpointStart (item) {
+    process.stdout.write(renderPrefix(item) + colors.gray('processing...'))
+  },
+  endpointEnd: function endpointEnd (state) {
+    const item = state.item
+    let error = state.error
+    process.stdout.clearLine && process.stdout.clearLine()
+    process.stdout.cursorTo && process.stdout.cursorTo(0)
+    process.stdout.write(renderPrefix(item))
+    if (error) {
+      console.log(colors.red('ERROR'))
+      error = processStack(error)
+      console.log('\n' + indent(error, '    ') + '\n')
+    } else {
+      console.log(colors.green('OK'))
+    }
+  }
+}
--- output/none.js
+++ output/none.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict'
 
-function skip() {
-	return undefined;
+function skip () {
+  return undefined
 }
 
 module.exports = {
-	start: skip,
-	end: function end(passed, total, cb) {
-		if (cb) {
-			cb(null, passed === total);
-		}
-	},
-	endpointStart: skip,
-	endpointEnd: skip
-};
+  start: skip,
+  end: function end (passed, total, cb) {
+    if (cb) {
+      cb(null, passed === total)
+    }
+  },
+  endpointStart: skip,
+  endpointEnd: skip
+}
